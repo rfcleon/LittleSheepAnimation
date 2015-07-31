beff69a01e093ce99a2125272207f9277c148256
mykoma <23775517@qq.com>
2015-07-31 23:00:38 +0800

    Add the first animation.

---
+++ LittleSheepAnimation/LittleSheepAnimation/Views/MyView.h
@@ -0,0 +1,13 @@
+//
+//  MyView.h
+//  TestChangex
+//
+//  Created by mykoma on 15/7/25.
+//  Copyright (c) 2015年 mykoma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyView : UIView
+
+@end
---
+++ LittleSheepAnimation/LittleSheepAnimation/Views/MyView.m
@@ -0,0 +1,45 @@
+//
+//  MyView.m
+//  TestChangex
+//
+//  Created by mykoma on 15/7/25.
+//  Copyright (c) 2015年 mykoma. All rights reserved.
+//
+
+#import "MyView.h"
+
+@interface MyView ()
+
+@property (nonatomic, strong) IBOutlet NSLayoutConstraint * constraint1;
+@property (nonatomic, strong) IBOutlet UIView * myView;
+@property (nonatomic, strong) IBOutlet NSLayoutConstraint * constraint2;
+@property (nonatomic, strong) IBOutlet NSLayoutConstraint * constraint3;
+
+@end
+
+@implementation MyView
+
+- (void)awakeFromNib
+{
+    [super awakeFromNib];
+}
+
+- (IBAction)touchOn:(id)sender
+{
+    self.constraint1.constant = 80;
+    [self removeConstraint:self.constraint2];
+    self.constraint3.constant = 100;
+
+    [UIView animateWithDuration:1.5
+                          delay:0
+         usingSpringWithDamping:0.4
+          initialSpringVelocity:0
+                        options:UIViewAnimationOptionCurveEaseOut
+                     animations:^{
+                         [self layoutIfNeeded];
+                     } completion:^(BOOL finished) {
+                         
+                     }];
+}
+
+@end
