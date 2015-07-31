d670961fa9e5128ecce0dcb4a54a1846f1257b5d
mykoma <23775517@qq.com>
2015-07-31 17:43:32 +0800

    Submit the project.

---
+++ LittleSheepAnimation/LittleSheepAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LittleSheepAnimation
+//
+//  Created by admin on 15/7/31.
+//  Copyright (c) 2015年 SCU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LittleSheepAnimation/LittleSheepAnimation/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  LittleSheepAnimation
+//
+//  Created by admin on 15/7/31.
+//  Copyright (c) 2015年 SCU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
