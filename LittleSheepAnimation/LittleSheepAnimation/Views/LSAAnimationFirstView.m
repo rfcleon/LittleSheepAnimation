20828ecd21daefd722f64bee366d8522c7ee994e
mykoma <23775517@qq.com>
2015-08-02 22:28:19 +0800

    Add the Animation for Close

--- LittleSheepAnimation/LittleSheepAnimation/Views/LSAAnimationFirstView.m
+++ LittleSheepAnimation/LittleSheepAnimation/Views/LSAAnimationFirstView.m
@@ -10,10 +10,10 @@
 
 @interface LSAAnimationFirstView ()
 
-@property (nonatomic, strong) IBOutlet NSLayoutConstraint * constraint1;
 @property (nonatomic, strong) IBOutlet UIView * myView;
-@property (nonatomic, strong) IBOutlet NSLayoutConstraint * constraint2;
-@property (nonatomic, strong) IBOutlet NSLayoutConstraint * constraint3;
+@property (nonatomic, strong) IBOutlet NSLayoutConstraint * topViewHeightConstraint;
+@property (nonatomic, strong) IBOutlet NSLayoutConstraint * bottomViewRelateTopViewConstraint;
+@property (nonatomic, strong) IBOutlet NSLayoutConstraint * openWitdhConstraint;
 
 @end
 
@@ -24,22 +24,54 @@
     [super awakeFromNib];
 }
 
-- (IBAction)touchOn:(id)sender
+- (IBAction)open:(id)sender
 {
-    self.constraint1.constant = 80;
-    [self removeConstraint:self.constraint2];
-    self.constraint3.constant = 100;
+    self.topViewHeightConstraint.constant = 100;
+    [self removeConstraint:self.bottomViewRelateTopViewConstraint];
+    NSLayoutConstraint * changedOpenWidthConstraint = [NSLayoutConstraint constraintWithItem:self.openWitdhConstraint.firstItem
+                                                                                   attribute:self.openWitdhConstraint.firstAttribute
+                                                                                   relatedBy:self.openWitdhConstraint.relation
+                                                                                      toItem:self.openWitdhConstraint.secondItem
+                                                                                   attribute:self.openWitdhConstraint.secondAttribute
+                                                                                  multiplier:1.0/4
+                                                                                    constant:self.openWitdhConstraint.constant];
+    [self removeConstraint:self.openWitdhConstraint];
+    self.openWitdhConstraint = changedOpenWidthConstraint;
+    [self addConstraint:self.openWitdhConstraint];
 
-    [UIView animateWithDuration:1.5
+    [UIView animateWithDuration:1
                           delay:0
-         usingSpringWithDamping:0.4
+         usingSpringWithDamping:0.3
           initialSpringVelocity:0
                         options:UIViewAnimationOptionCurveEaseOut
                      animations:^{
                          [self layoutIfNeeded];
-                     } completion:^(BOOL finished) {
-                         
-                     }];
+                     } completion:NULL];
+}
+
+- (IBAction)close:(id)sender
+{
+    self.topViewHeightConstraint.constant = 200;
+    [self addConstraint:self.bottomViewRelateTopViewConstraint];
+    NSLayoutConstraint * changedOpenWidthConstraint = [NSLayoutConstraint constraintWithItem:self.openWitdhConstraint.firstItem
+                                                                                   attribute:self.openWitdhConstraint.firstAttribute
+                                                                                   relatedBy:self.openWitdhConstraint.relation
+                                                                                      toItem:self.openWitdhConstraint.secondItem
+                                                                                   attribute:self.openWitdhConstraint.secondAttribute
+                                                                                  multiplier:3.0/4
+                                                                                    constant:self.openWitdhConstraint.constant];
+    [self removeConstraint:self.openWitdhConstraint];
+    self.openWitdhConstraint = changedOpenWidthConstraint;
+    [self addConstraint:self.openWitdhConstraint];
+    
+    [UIView animateWithDuration:1
+                          delay:0
+         usingSpringWithDamping:0.3
+          initialSpringVelocity:0
+                        options:UIViewAnimationOptionCurveEaseOut
+                     animations:^{
+                         [self layoutIfNeeded];
+                     } completion:NULL];
 }
 
 @end
